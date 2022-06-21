5fd282e6f5339c61501481ef45600c497bbb83e4
Michael <Michael@ZPF.fr>
2022-06-21 23:52:40 +0200

    Initial CheckIn ....

---
+++ Sources/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string _path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+_path += @"\.nuget\packages\";
+
+Console.WriteLine("NugetsCleaner");
+Console.WriteLine("");
+Console.WriteLine(_path);
+
+var packages = System.IO.Directory.GetDirectories(_path);
+
+foreach (var package in packages)
+{
+    // Console.WriteLine(System.IO.Path.GetFileName(package));
+
+    var versions = System.IO.Directory.GetDirectories(package);
+
+    if (versions.Length > 1)
+    {
+        Console.WriteLine(System.IO.Path.GetFileName(package));
+
+        for (int i = 0; i < versions.Length - 1; i++)
+        {
+            System.IO.Directory.Delete(versions[i], true);
+        };
+    };
+};
